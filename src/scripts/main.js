e6faf8ef49c9496b919f6f4a2b86facc0a1bd28e
hanax <alfalfa.xue@gmail.com>
2015-12-06 17:11:18 -0500

    add hamburger menu, fix animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,30 +2,42 @@ import 'normalize-css';
 import '../styles/index.styl';
 
 $(() => {
-  $('.slide')
-    .css("min-height", $(window).height());
+  const windowHeight = $(window).height();
+  $('.slide').css("min-height", windowHeight);
 
   $(window).scroll(() => {
-    if ($("body").scrollTop() > 200) {
+    if ($("body").scrollTop() > windowHeight / 2) {
       $(".header").addClass("stick");
     } else {
       $(".header").removeClass("stick");
+      $(".menu").fadeOut("fast");
+      $(".menu-dropdown").fadeOut("fast");
+    }
+  });
+
+  $(".menu").click(() => {
+    $(".menu-dropdown").fadeToggle("fast");
+  });
+
+  $("body").click((e) => {
+    const t = $(e.target);
+    if (!t.closest(".menu").length && !t.closest(".menu-dropdown").length) {
+      console.log('yo')
+      $(".menu-dropdown").fadeOut("fast");
     }
   });
 
   // Smooth Scrolling
-  $(() => {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 700);
-          return false;
-        }
+  $('a[href*=#]:not([href=#])').click(() => {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
       }
-    });
+    }
   });
 });
