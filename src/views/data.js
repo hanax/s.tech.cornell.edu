c05b1a61b9de6419bc40c72e30df992f2bdedce8
hanax <alfalfa.xue@gmail.com>
2016-02-02 20:02:17 -0500

    Change publications layout

--- src/views/data.js
+++ src/views/data.js
@@ -2,44 +2,76 @@ var marked = require('marked');
 
 var publications = [
   {
-    title: 'Nir Grinberg, P. Alex Dow, Lada A. Adamic, and Mor Naaman. Changes in Engagement Before and After Posting to Facebook. In Proceedings, ACM Conference on Human Factors in Computing Systems. (CHI 2016), May 2015, San Jose, USA',
+    mobileInfo: 'Nir Grinberg, P. Alex Dow, Lada A. Adamic, and Mor Naaman. Changes in Engagement Before and After Posting to Facebook. In Proceedings, ACM Conference on Human Factors in Computing Systems. (CHI 2016), May 2015, San Jose, USA',
+    title: 'Changes in Engagement Before and After Posting to Facebook',
+    author: 'Nir Grinberg, P. Alex Dow, Lada A. Adamic, and Mor Naaman',
+    conference: 'CHI 2016',
     image: '/assets/img/publication/publication-08.png',
-    link: 'http://www.nirg.net/papers/contribution2016grinberg.pdf'
+    link: 'http://www.nirg.net/papers/contribution2016grinberg.pdf',
+    abstract: 'The asynchronous nature of communications on social network sites creates a unique opportunity for studying how posting content interacts with individuals’ engagement. This study focuses on the behavioral changes occurring hours before and after contribution to better understand the changing needs and preferences of contributors. Using observational data analysis of individuals’ activity on Facebook, we test hypotheses regarding the motivations for site visits, changes in the distribution of attention to content, and shifts in decisions to interact with others. We find that after posting content people are intrinsically motivated to visit the site more often, are more attentive to content from friends (but not others), and choose to interact more with friends (in large part due to reciprocity). In addition, contributors are more active on the site hours before posting and remain more active for less than a day afterwards. Our study identifies a unique pattern of engagement that accompanies contribution and can inform the design of social network sites to better support contributors.'
   },
   {
-    title: 'Raz Schwarz, Mor Naaman, Rannie Teodoro. Editorial Algorithms: Using Social Media to Discover and Report Local News. In Proceedings, ACM International Conference on Web Logs and Social Media. (ICWSM 2015), May 2015, Cambridge, England',
+    mobileInfo: 'Raz Schwarz, Mor Naaman, Rannie Teodoro. Editorial Algorithms: Using Social Media to Discover and Report Local News. In Proceedings, ACM International Conference on Web Logs and Social Media. (ICWSM 2015), May 2015, Cambridge, England',
+    title: 'Editorial Algorithms: Using Social Media to Discover and Report Local News',
+    author: 'Raz Schwarz, Mor Naaman, Rannie Teodoro',
+    conference: 'ICWSM 2015',
     image: '/assets/img/publication/publication-01.png',
-    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Schwartz_ICWSM2015_CityBeat.pdf'
+    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Schwartz_ICWSM2015_CityBeat.pdf',
+    abstract: 'The role of algorithms in the detection, curation and broadcast of news is becoming increasingly prevalent. To better understand this role we developed CityBeat, a system that implements what we call “editorial algorithms” to find possible news events. This fully functional system collects real time geo-tagged information from social media, finds key stories, makes an editorial decision whether these events are of interest and eventually visualizes these stories on a big screen display. The system was designed in collaboration with journalists and tested at four New York City newsrooms. Our results show that while journalists were receptive to the idea of machine-generated news stories, the actual results of the system confirmed current concerns among journalists and researchers about the dangers of outsourcing news finding tasks to machines. This paper therefore exemplifies how news sourcing systems based on social media may favor specific type of news events, do not report results quickly enough, and cater to a biased population and interests.'
   },
   {
-    title: 'Minsu Park, Ingmar Weber, Mor Naaman, Sarah Vieweg. Understanding Musical Diversity via Online Social Media. In Proceedings, ACM International Conference on Web Logs and Social Media. (ICWSM 2015), May 2015, Cambridge, England',
+    mobileInfo: 'Minsu Park, Ingmar Weber, Mor Naaman, Sarah Vieweg. Understanding Musical Diversity via Online Social Media. In Proceedings, ACM International Conference on Web Logs and Social Media. (ICWSM 2015), May 2015, Cambridge, England',
+    title: 'Understanding Musical Diversity via Online Social Media',
+    author: 'Minsu Park, Ingmar Weber, Mor Naaman, Sarah Vieweg',
+    conference: 'ICWSM 2015',
     image: '/assets/img/publication/publication-02.png',
-    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Park_ICWSM2015_MusicalDiversity.pdf'
+    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Park_ICWSM2015_MusicalDiversity.pdf',
+    abstract: 'Musicologists and sociologists have long been interested in patterns of music consumption and their relation to socioeconomic status. In particular, the Omnivore Thesis examines the relationship between these variables and the diversity of music a person consumes. Using data from social media users of Last.fm and Twitter, we design and evaluate a measure that reasonably captures diversity of musical tastes. We use that measure to explore associations between musical diversity and variables that capture socioeconomic status, demographics, and personal traits such as openness and degree of interest in music (into-ness). Our musical diversity measure can provide a useful means for studies of musical preferences and consumption. Also, our study of the Omnivore Thesis provides insights that extend previous survey and interview-based studies.'
   },
   {
-    title: 'David Flatow, Mor Naaman, Ke Eddie Xie, Yana Volkovich, Yaron Kanza. On the Accuracy of Hyper-local Geotagging of Social Media Content. In Proceedings, the ACM International Conference on Web Search and Data Mining (WSDM 2015). March 2015, Shanghai, China.',
+    mobileInfo: 'David Flatow, Mor Naaman, Ke Eddie Xie, Yana Volkovich, Yaron Kanza. On the Accuracy of Hyper-local Geotagging of Social Media Content. In Proceedings, the ACM International Conference on Web Search and Data Mining (WSDM 2015). March 2015, Shanghai, China.',
+    title: 'On the Accuracy of Hyper-local Geotagging of Social Media Content',
+    author: 'David Flatow, Mor Naaman, Ke Eddie Xie, Yana Volkovich, Yaron Kanza',
+    conference: 'WSDM 2015',
     image: '/assets/img/publication/publication-03.png',
-    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/flatow_wsdm2015_geocoding.pdf'
+    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/flatow_wsdm2015_geocoding.pdf',
+    abstract: 'Social media users share billions of items per year, only a small fraction of which is geotagged. We present a datadriven approach for identifying non-geotagged content items that can be associated with a hyper-local geographic area by modeling the location distributions of n-grams that appear in the text. We explore the trade-off between accuracy and coverage of this method. Further, we explore differences across content received from multiple platforms and devices, and show, for example, that content shared via different sources and applications produces significantly different geographic distributions, and that it is preferred to model and predict location for items according to their source. Our findings show the potential and the bounds of a data-driven approach to assigning location data to short social media texts, and offer implications for all applications that use data-driven approaches to locate content.'
   },
   {
-    title: 'Funda Kivran-Swaine, Jeremy Ting, Jed Richards Brubaker, Rannie Teodoro, Mor Naaman.Understanding Loneliness in Social Awareness Streams: Expressions and Responses. Eighth International AAAI Conference on Weblogs and Social Media. (ICWSM 2014)',
+    mobileInfo: 'Funda Kivran-Swaine, Jeremy Ting, Jed Richards Brubaker, Rannie Teodoro, Mor Naaman.Understanding Loneliness in Social Awareness Streams: Expressions and Responses. Eighth International AAAI Conference on Weblogs and Social Media. (ICWSM 2014)',
+    title: 'Understanding Loneliness in Social Awareness Streams: Expressions and Responses',
+    author: 'Funda Kivran-Swaine, Jeremy Ting, Jed Richards Brubaker, Rannie Teodoro, Mor Naaman',
+    conference: 'ICWSM 2014',
     image: '/assets/img/publication/publication-04.png',
-    link: 'http://www.aaai.org/ocs/index.php/ICWSM/ICWSM14/paper/view/8038/8126'
+    link: 'http://www.aaai.org/ocs/index.php/ICWSM/ICWSM14/paper/view/8038/8126',
+    abstract: 'We studied the experience of loneliness as communicated by thousands of people on Twitter. Using a data set of public Twitter posts containing explicit expressions of loneliness, we qualitatively developed a categorization scheme for these expressions, showing how the context of loneliness expressed on Twitter relates to existing theories about loneliness. A quantitative analysis of the data exposed categories and patterns in communication practices around loneliness. For example, users expressing more severe, enduring loneliness are more likely to be female, and less likely to include requests for social interaction in their tweets. Further, we studied the responses to expressions of loneliness in Twitter’s social settings. Deriving from the same dataset, we examined factors that correlate with the existence and type of response, showing, for example, that men were more likely to receive responses to lonely tweets, and expressions of enduring loneliness are critically less likely to receive responses.'
   },
   {
-    title: 'Rannie Teodoro, Pinar Ozturk, Mor Naaman, Winter Mason, and Janne Lindqvist. The Motivations and Experiences of the On-demand Mobile Workforce. In Proceedings, ACM Conference on Computer Supported Cooperative Work. (CSCW 2014), February 2014',
+    mobileInfo: 'Rannie Teodoro, Pinar Ozturk, Mor Naaman, Winter Mason, and Janne Lindqvist. The Motivations and Experiences of the On-demand Mobile Workforce. In Proceedings, ACM Conference on Computer Supported Cooperative Work. (CSCW 2014), February 2014',
+    title: 'The Motivations and Experiences of the On-demand Mobile Workforce',
+    author: 'Rannie Teodoro, Pinar Ozturk, Mor Naaman, Winter Mason, and Janne Lindqvist',
+    conference: 'CSCW 2014',
     image: '/assets/img/publication/publication-05.png',
-    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Teodoro_CSCW2014_mobile_workforce.pdf'
+    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Teodoro_CSCW2014_mobile_workforce.pdf',
+    abstract: 'On-demand mobile workforce applications match physical world tasks and willing workers. These systems offer to help conserve resources, streamline courses of action, and increase market efficiency for micro- and mid-level tasks, from verifying the existence of a pothole to walking a neighbor’s dog. This study reports on the motivations and experiences of individuals who regularly complete physical world tasks posted in on-demand mobile workforce marketplaces. Data collection included semi-structured interviews with members (workers) of two different services. The analysis revealed the main drivers for participating in an on-demand mobile workforce, including desires for monetary compensation and control over schedules and task selection. We also reveal main reasons for task selection, which involve situational factors, convenient physical locations, and task requester profile information. Finally, we discuss the key characteristics of the most worthwhile tasks and offer implications for novel crowdsourcing systems for physical world tasks.'
   },
   {
-    title: 'Jessa Lingel, Mor Naaman and danah boyd. City, Self, Network: Transnational Migrants and Online Identity Work. In Proceedings, ACM Conference on Computer Supported Cooperative Work. (CSCW 2014), February 2014',
+    mobileInfo: 'Jessa Lingel, Mor Naaman and danah boyd. City, Self, Network: Transnational Migrants and Online Identity Work. In Proceedings, ACM Conference on Computer Supported Cooperative Work. (CSCW 2014), February 2014',
+    title: 'City, Self, Network: Transnational Migrants and Online Identity Work',
+    author: 'Jessa Lingel, Mor Naaman and danah boyd',
+    conference: 'CSCW 2014',
     image: '/assets/img/publication/publication-06.jpg',
-    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Lingel_CSCW2014_migrants.pdf'
+    link: 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Lingel_CSCW2014_migrants.pdf',
+    abstract: 'We use qualitative interviews with 26 transnational migrants in New York City to analyze socio-technical practices related to online identity work. We focus specifically on the use of Facebook, where benefits included keeping in touch with friends and family abroad and documenting everyday urban life. At the same time, many participants also reported experiences of fatigue, socio-cultural tensions and concerns about maintaining a sense of personal privacy. These experiences highlight how transnational practices complicate context collapse, where the geographic dispersal of participants’ personal networks renders visible conflicts of “flattened” online networks. Our findings also suggest a kind of technology-enabled codeswitching, where transnational migrants leverage social media to perform identities that alternate between communities, nationalities and geographies. This analysis informs HCI research on transnationalism and technological practices, and helps expose the complexities of online identity work in terms of shifting social and spatial contexts.'
   },
   {
-    title: 'Grinberg, Nir, Mor Naaman, Blake Shaw, and Gilad Lotan. "Extracting Diurnal Patterns of Real World Activity from Social Media." In ICWSM. 2013.',
+    mobileInfo: 'Grinberg, Nir, Mor Naaman, Blake Shaw, and Gilad Lotan. "Extracting Diurnal Patterns of Real World Activity from Social Media." In ICWSM. 2013.',
+    title: 'Extracting Diurnal Patterns of Real World Activity from Social Media',
+    author: 'Grinberg, Nir, Mor Naaman, Blake Shaw, and Gilad Lotan',
+    conference: 'ICWSM 2013',
     image: '/assets/img/publication/publication-07.png',
-    link: 'http://www.nirg.net/papers/grinberg-icwsm2013-extracting.pdf'
+    link: 'http://www.nirg.net/papers/grinberg-icwsm2013-extracting.pdf',
+    abstract: 'In this study, we develop methods to identify verbal expressions in social media streams that refer to real-world activities. Using aggregate daily patterns of Foursquare checkins, our methods extract similar patterns from Twitter, extending the amount of available content while preserving high relevance. We devise and test several methods to extract such content, using timeseries and semantic similarity. Evaluating on key activity categories available from Foursquare (coffee, food, shopping and nightlife), we show that our extraction methods are able to capture equivalent patterns in Twitter. By examining rudimentary categories of activity such as nightlife, food or shopping we peek at the fundamental rhythm of human behavior and observe when it is disrupted. We use data compiled during the abnormal conditions in New York City throughout Hurricane Sandy to examine the outcome of our methods.'
   },
 ];
 
