a8bbc1fba52143afdc60b3a736f2b8a1320f26b6
Emily Sun <es765@cornell.edu>
2017-01-25 09:13:02 -0500

    updated w/chi

--- src/views/data.js
+++ src/views/data.js
@@ -1,25 +1,54 @@
 var marked = require('marked');
 
 var publications = [
+  { 
+  mobileInfo: 'Max Grusky, Jeiran Jahani, Josh Schwartz, Dan Valente, Yoav Artzi, and Mor Naaman. Modeling Sub-Document Attention Using Viewport Time',
+    title: 'Modeling Sub-Document Attention Using Viewport Time',
+    author: 'Max Grusky, Jeiran Jahani, Josh Schwartz, Dan Valente, Yoav Artzi, and Mor Naaman',
+    conference: 'CHI 2017',
+    type: 'Note',
+    image: '/assets/img/publication/viewport-time-chi2017.png',
+    links: [['PDF', '/assets/papers/viewport-time-chi2017.pdf']],
+    abstract: 'Website measures of engagement captured from millions of users, such as in-page scrolling and viewport position, can provide deeper understanding of attention than possible with simpler measures, such as dwell time. Using data from 1.2M news reading sessions, we examine and evaluate three increasingly sophisticated models of sub-document attention computed from viewport time, the time a page component is visible on the user display. Our approach supports refined large-scale measurement of user engagement at a level previously available only from lab-based eye-tracking studies.'
+  },
  { 
-  
+  mobileInfo: 'Emily Sun, Ross McLachlan, Mor Naaman. MoveMeant: Anonymously Building Community Through Shared Location Histories.',
+    title: 'MoveMeant: Anonymously Building Community Through Shared Location Histories',
+    author: 'Emily Sun*, Ross McLachlan*, Mor Naaman (*authors contributed equally)',
+    conference: 'CHI 2017',
+    type: 'Note',
+    image: '/assets/img/publication/movemeant.png',
+    links: [['PDF', '/assets/papers/movemeant-note-chi2017.pdf']],
+    abstract: 'We describe the design and implementation of MoveMeant, a system aimed to increase local community awareness through shared location traces. We report findings from interviews with residents in the Bronx, New York City who participated in a deployment of MoveMeant over a 6-week period.'
+  },
+ { 
+  mobileInfo: 'Xiao Ma, Nazanin Andalibi, Louise Barkhuus, Mor Naaman. “People Are Either Too Fake or Too Real”: Opportunities and Challenges in Tie-Based Anonymity',
+    title: '“People Are Either Too Fake or Too Real”: Opportunities and Challenges in Tie-Based Anonymity',
+    author: 'Xiao Ma, Nazanin Andalibi, Louise Barkhuus, Mor Naaman',
+    conference: 'CHI 2017',
+    type: 'Paper',
+    image: '/assets/img/publication/secret-qual.png',
+    links: [['PDF', '/assets/papers/secret-qual.pdf']],
+    abstract: 'In recent years, several mobile applications allowed individuals to anonymously share information with friends and contacts, without any persistent identity marker. The functions of these “tie-based” anonymity services may be notably different than other social media services. We use semi-structured interviews to qualitatively examine motivations, practices and perceptions in two tie-based anonymity apps: Secret (now defunct, in the US) and Mimi (in China).'
+  },
+ { 
   mobileInfo: 'Xiao Ma, Jeff Hancock, Kenneth Lim Mingjie, Mor Naaman. Self-Disclosure and Perceived Trustworthiness of Airbnb Host Profiles',
     title: 'Self-Disclosure and Perceived Trustworthiness of Airbnb Host Profiles',
     author: 'Xiao Ma, Jeff Hancock, Kenneth Lim Mingjie, Mor Naaman',
     conference: 'CSCW 2017',
-    type: 'Full Paper',
+    isAward: 'Best Paper Honorable Mention',
+    type: 'Paper',
     image: '/assets/img/publication/airbnb.png',
     links: [['Dataset', 'https://github.com/sTechLab/AirbnbHosts', 'arrow-down'], 
             ['PDF', '/assets/papers/ma2017airbnb.pdf']],
     abstract: 'We examine how hosts describe themselves on their Airbnb profile pages. We use a mixed-methods study to develop a categorization of the topics that hosts self-disclose in their profile descriptions, and show that these topics differ depending on the type of guest engagement expected. We also examine the perceived trustworthiness of profiles using topic-coded profiles from 1,200 hosts, showing that longer self-descriptions are perceived to be more trustworthy. Further, we show that there are common strategies (a mix of topics) hosts use in self-disclosure, and that these strategies cause differences in perceived trustworthiness scores. Finally, we show that the perceived trustworthiness score is a significant predictor of host choice--especially for shorter profiles that show more variation.'
   },
  { 
-  
  	mobileInfo: 'Emily Sun, Ross McLachlan, Mor Naaman. TAMIES: A Study and Model of Adoption in P2P Resource Sharing and Indirect Exchange Systems',
     title: 'TAMIES: A Study and Model of Adoption in P2P Resource Sharing and Indirect Exchange Systems',
-    author: 'Emily Sun, Ross McLachlan, Mor Naaman',
+    author: 'Emily Sun*, Ross McLachlan*, Mor Naaman (*authors contributed equally)',
     conference: 'CSCW 2017',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/P2PTAM_CR.jpg',
     links: [['PDF', '/assets/papers/tamies.pdf']],
     abstract: 'Peer-to-peer indirect exchange services, such as Peerby and NeighborGoods, do not seem to have been as widely adopted as direct exchange systems, such as Uber and AirBnb. Building upon the results of interviews with 37 residents of New York City, a survey with 195 respondents, previous technology acceptance models, critical mass theory, and prior research on peer economies, we propose a technology acceptance model for indirect exchange systems that includes generalized trust and ease of coordination.'
@@ -29,7 +58,7 @@ var publications = [
     title: 'What Happens in happn: The Warranting Power of Location History in Online Dating',
     author: 'Xiao Ma, Emily Sun, Mor Naaman',
     conference: 'CSCW 2017',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/happn_website.jpg',
     links: [['PDF', '/assets/papers/making-sense-location_CR.pdf']],
     abstract: 'A recent mobile dating application, happn, adds a temporal dimension to location-based dating, showing users the number of times and recent overlap that they crossed path with each other. We conducted qualitative interviews with 15 happn users and discuss the findings in the context of Uncertainty Reduction Theory. The warranting power of the device driven location data was accepted as valuable and generated little concern about misrepresentation. Our findings suggest the potential for utilizing location data outside of the domain of online dating.'
@@ -40,7 +69,7 @@ var publications = [
     title: 'A Data-driven Study of View Duration on YouTube',
     author: 'Minsu Park, Mor Naaman, Jonah Berger',
     conference: 'ICWSM 2016',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-youtube.jpg',
     links: [['Dataset', 'https://github.com/sTechLab/YouTubeDurationData', 'arrow-down'], 
             ['PDF', '/assets/papers/ParkICWSM2016_YT.pdf']],
@@ -51,7 +80,7 @@ var publications = [
     title: 'Anonymity, Intimacy and Self-Disclosure in Social Media',
     author: 'Xiao Ma, Jeff Hancock, Mor Naaman',
     conference: 'CHI 2016',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-Anonymity.jpg',
     video: '/assets/img/publication/publication-Anonymity.mp4',
     links: [['Dataset', 'https://github.com/sTechLab/SelfDisclosureItems', 'arrow-down'], 
@@ -64,7 +93,7 @@ var publications = [
     author: 'Nir Grinberg, P. Alex Dow, Lada A. Adamic, and Mor Naaman',
     conference: 'CHI 2016',
     isAward: 'Best Paper Honorable Mention',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-08.png',
     links: [['PDF', 'http://www.nirg.net/papers/contribution2016grinberg.pdf']],
     abstract: 'The study identified a unique pattern of engagement that accompanies posting on Facebook. Using observational data analysis we show that after posting content, people visit the site more often, are more attentive to content from friends, and even interact more with friends content.'
@@ -74,7 +103,7 @@ var publications = [
     title: 'Editorial Algorithms: Using Social Media to Discover and Report Local News',
     author: 'Raz Schwarz, Mor Naaman, Rannie Teodoro',
     conference: 'ICWSM 2015',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-01.png',
     links: [['PDF', 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Schwartz_ICWSM2015_CityBeat.pdf']],
     abstract: 'We discuss CityBeat, a large-screen visualization that builds on machine learning techniques to expose hyper-local events in New York City from social media data. We deployed it with some national and local media and describe the gap between the journalistic needs and what our algorithm could provide.'
@@ -84,7 +113,7 @@ var publications = [
     title: 'Understanding Musical Diversity via Online Social Media',
     author: 'Minsu Park, Ingmar Weber, Mor Naaman, Sarah Vieweg',
     conference: 'ICWSM 2015',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-02.png',
     links: [['PDF', 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Park_ICWSM2015_MusicalDiversity.pdf']],
     abstract: 'Using data from users of Last.fm and Twitter, we design and evaluate a novel measure for computing diversity of musical tastes, and investigate its relationships with socioeconomic status and personal traits such as openness and degree of interest in music.'
@@ -94,7 +123,7 @@ var publications = [
     title: 'On the Accuracy of Hyper-local Geotagging of Social Media Content',
     author: 'David Flatow, Mor Naaman, Ke Eddie Xie, Yana Volkovich, Yaron Kanza',
     conference: 'WSDM 2015',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-03.png',
     links: [['PDF', 'https://people.jacobs.cornell.edu/mor/publications/thegoods/flatow_wsdm2015_geocoding.pdf']],
     abstract: 'Can we use geotagged social media to identify phrases that correspond to hyper-local geographic features? Does the geographic spread of such phrases differ between services or device types? (the answer to both is yes).'
@@ -104,7 +133,7 @@ var publications = [
     title: 'Understanding Loneliness in Social Awareness Streams: Expressions and Responses',
     author: 'Funda Kivran-Swaine, Jeremy Ting, Jed Richards Brubaker, Rannie Teodoro, Mor Naaman',
     conference: 'ICWSM 2014',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-04.png',
     links: [['PDF', 'http://www.aaai.org/ocs/index.php/ICWSM/ICWSM14/paper/view/8038/8126']],
     abstract: 'Content analysis of tweets expressing loneliness exposes some key patterns in how people talk about loneliness on Twitter, as well as when such posts are likely to receive a response from friends and followers. '
@@ -114,7 +143,7 @@ var publications = [
     title: 'The Motivations and Experiences of the On-demand Mobile Workforce',
     author: 'Rannie Teodoro, Pinar Ozturk, Mor Naaman, Winter Mason, and Janne Lindqvist',
     conference: 'CSCW 2014',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-05.png',
     links: [['PDF', 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Teodoro_CSCW2014_mobile_workforce.pdf']],
     abstract: 'We performed semi-structured interviews with workers of on-demand mobile workforce applications that match users with physical world tasks to expose their motivations, practices, and experiences. '
@@ -124,7 +153,7 @@ var publications = [
     title: 'City, Self, Network: Transnational Migrants and Online Identity Work',
     author: 'Jessa Lingel, Mor Naaman and danah boyd',
     conference: 'CSCW 2014',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-06.jpg',
     links: [['PDF', 'https://people.jacobs.cornell.edu/mor/publications/thegoods/Lingel_CSCW2014_migrants.pdf']],
     abstract: 'What are the challenges new immigrants face in online self-presentation and use of Facebook? We did a qualitative study to find out. '
@@ -134,7 +163,7 @@ var publications = [
     title: 'Extracting Diurnal Patterns of Real World Activity from Social Media',
     author: 'Grinberg, Nir, Mor Naaman, Blake Shaw, and Gilad Lotan',
     conference: 'ICWSM 2013',
-    type: 'Full Paper',
+    type: 'Paper',
     image: '/assets/img/publication/publication-07.png',
     links: [['PDF', 'http://www.nirg.net/papers/grinberg-icwsm2013-extracting.pdf']],
     abstract: 'We developed methods to identify how people talk about real-world activities on social media. By examining activities such as nightlife, food or shopping we peek at the fundamental rhythm of human behavior at a city level and observe how it was disrupted during Hurricane Sandy.'
@@ -226,15 +255,23 @@ var people = [
 
 var news = [
 	{ 
-		title: 'Five papers by lab authors accepted to CSCW 2017 -- PDFs available soon!',
+		title: '“Self-Disclosure and Perceived Trustworthiness of Airbnb Host Profiles” Honorable Mention for Best Paper at CSCW 2017',
+		date: '1/2017'
+	},
+	{ 
+		title: 'Three papers from the lab were accepted to CHI. Check out the PDFs below!',
+		date: '12/2016'
+	},
+	{ 
+		title: 'Five papers by lab authors accepted to CSCW 2017 -- PDF now available.',
 		date: '9/2016'
 	},
-	{
+/*	{
 		title:' “Changes in Engagement Before and After Posting to Facebook”  Honorable Mention for Best Paper at CHI 2016',
 		date:'5/2016'
 	},
   	{
-    	title: 'Paper studying view duration of YouTube videos to appear in ICWSM; the paper and dataset are both available below',
+/*    	title: 'Paper studying view duration of YouTube videos to appear in ICWSM; the paper and dataset are both available below',
     	date: '4/2016'
   	},
 /*  {
